2ac5c9d3d250783f4a567f05169a1f3662aa5ff4
ppyz8509 <peenp1234@gmail.com>
2023-11-14 22:07:34 +0700

    calculator-react-TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+interface BMIForm {
+  height: number;
+  weight: number;
+}
+
+const App: React.FC = () => {
+  const [formData, setFormData] = useState<BMIForm>({ height: 0, weight: 0 });
+  const [bmi, setBMI] = useState<number | null>(null);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, field: keyof BMIForm) => {
+    setFormData({ ...formData, [field]: parseFloat(e.target.value) || 0 });
+  };
+
+  const calculateBMI = () => {
+    const { height, weight } = formData;
+    if (height > 0 && weight > 0) {
+      const heightInMeters = height / 100; // Convert height to meters
+      const bmiValue = weight / (heightInMeters * heightInMeters);
+      setBMI(bmiValue);
+    } else {
+      setBMI(null);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>BMI Calculator</h1>
+      <div>
+        <label htmlFor="height">Height (cm):</label>
+        <input
+          type="number"
+          id="height"
+          value={formData.height}
+          onChange={(e) => handleInputChange(e, 'height')}
+        />
+      </div>
+      <div>
+        <label htmlFor="weight">Weight (kg):</label>
+        <input
+          type="number"
+          id="weight"
+          value={formData.weight}
+          onChange={(e) => handleInputChange(e, 'weight')}
+        />
+      </div>
+      <button onClick={calculateBMI}>Calculate BMI</button>
+      {bmi !== null && (
+        <div>
+          <h2>Your BMI: {bmi.toFixed(2)}</h2>
+          <p>
+            Interpretation:
+            {bmi < 18.5
+              ? 'Underweight'
+              : bmi < 24.9
+              ? 'Normal weight'
+              : bmi < 29.9
+              ? 'Overweight'
+              : 'Obese'}
+          </p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
